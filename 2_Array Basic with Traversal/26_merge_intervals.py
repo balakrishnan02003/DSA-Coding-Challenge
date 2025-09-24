576f7565a568317997ec7ddbf0045e2c8971ca9b
balakrishnan02003 <120874104+balakrishnan02003@users.noreply.github.com>
2025-09-25 01:09:26 +0530

    Update 26_merge_intervals.py

--- 2_Array Basic with Traversal/26_merge_intervals.py
+++ 2_Array Basic with Traversal/26_merge_intervals.py
@@ -1,3 +1,6 @@
+# Given an array of intervals where intervals[i] = [starti, endi], merge all overlapping intervals, and return an array of the non-overlapping intervals that cover all the intervals in the input.
+# Link: https://leetcode.com/problems/merge-intervals/description/
+# Approach: Sort the elements in the list. If the value of element's 1st index is higher than or equal to the value of element's 0th index of next element, then remove those elements and add value to the array at the current position, where value is element in the 0th index at the current position and maximum among element at 1st index in ith or i+1th index. 
 class Solution(object):
     def merge(self, intervals):
         """
@@ -5,11 +8,18 @@ class Solution(object):
         :rtype: List[List[int]]
         """
         intervals = sorted(intervals)
-        for i in range(len(intervals)-1):
-            if intervals[i[1]]> intervals[i+1[0]]:
-                arr = [intervals[i[0]],intervals[i+1[1]]]
+        i=0
+        while i<len(intervals)-1:
+            if intervals[i][1]>= intervals[i+1][0]:
+                arr = [intervals[i][0],max(intervals[i+1][1],intervals[i][1])]
                 intervals.pop(i)
-                intervals.pop(i+1)
+                intervals.pop(i) # Since the ith element is already popped, the i+1th element becomes ith element.
                 intervals.insert(i,arr)
+                
+            else:
+                i=i+1
         
-        return intervals
+        return intervals     
+
+soln = Solution()
+print(soln.merge([[1,3],[2,6],[8,10],[15,18]]))
