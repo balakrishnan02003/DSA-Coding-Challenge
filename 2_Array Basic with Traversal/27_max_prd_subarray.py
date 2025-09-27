b35aea40e3e1ef578b394ca6531e7585b3e27900
balakrishnan02003 <120874104+balakrishnan02003@users.noreply.github.com>
2025-09-27 17:02:06 +0530

    Update 27_max_prd_subarray.py

--- 2_Array Basic with Traversal/27_max_prd_subarray.py
+++ 2_Array Basic with Traversal/27_max_prd_subarray.py
@@ -1,6 +1,6 @@
 # Given an array arr[] that contains positive and negative integers (may contain 0 as well). Find the maximum product that we can get in a subarray of arr[].
 # Link: https://www.geeksforgeeks.org/problems/maximum-product-subarray3604/1
-# Approach: The intuition behind this is, the max product elements appear either in prefix or suffix of negative no. Hence we use prefix to operate no. for prefix(1st to last) and suffix(last to first). While processing each element, if current element is zero, that means, there starts a new subarray, hence reset the current multiplied value to 1 so to reset. Current maximum is the maximum value among current prefix,current suffix and previous maximum. To handle an edge case, where array has all negative no.s and zeros, we explicity check for it and return zero in that case; else if not that case, return current maximum after process all elements in the array.
+# Approach: The intuition behind this is, the max product elements appear either in prefix or suffix of negative no. Hence we use prefix to operate no. for prefix(1st to last) and suffix(last to first). While processing each element, if current element is zero, that means, there starts a new subarray, hence reset the current multiplied value to 1 so to reset. Current maximum is the maximum value among current prefix,current suffix and previous maximum. To handle an edge case, where array has only one zero and a negtaive no., we explicity check for it and return zero in this case; else if not this case, return current maximum after process all elements in the array.
 class Solution:
     def maxProduct(self, arr):
         # code here
@@ -8,7 +8,7 @@ class Solution:
         prefix = 1
         suffix = 1
         length = len(arr)
-        all_negative = True
+        has_negative = True
         has_zero = False
         
         for i in range(0, length):
@@ -16,7 +16,7 @@ class Solution:
             if arr[i]==0:
                 has_zero = True
             if arr[i]>0:
-                all_negative= False
+                has_negative= False
     
             if suffix == 0:
                 suffix = 1
@@ -30,7 +30,7 @@ class Solution:
             
             cur_max = max(cur_max, suffix, prefix)
         
-        if has_zero is True and all_negative is True:
+        if has_zero is True and has_negative is True and len(arr)==2:
             return 0
 
         return cur_max
